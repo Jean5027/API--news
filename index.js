aee2e200f39ddfef201b83204ea05d26bd543c89
Jean5027 <jeanviana5027@gmail.com>
2023-10-22 10:11:52 -0300

    refactor: add rout soma

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// ROTA
+  // Method HTTP - CRUD (create, read , update, delete)
+    //GET - pega a info
+    //POST - cria uma info
+    //PUT - altera uma info
+    //PATH - altera parte da info
+    //DELETE - apaga uma info
+
+  // Name - um identificador da rota
+
+
+  // Funcion (Callback) - Responsavel por executar algum comando
+
+app.get("/soma", (req, res) => {
+  const soma = 1 + 1;
+
+  res.send({soma: soma})
+  //res.json(soma)
+
+
+
+});
+
 
 app.listen(3000)
