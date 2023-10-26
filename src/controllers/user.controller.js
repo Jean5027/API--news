fbe2d03b7f01bb29c05c8b7e81f1f86c75c3d77f
Jean5027 <jeanviana5027@gmail.com>
2023-10-26 10:31:26 -0300

    feat: adicionando o crud

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,18 @@
 const userService = require('../services/user.service');
+const mongoose = require('mongoose')
+
 
 const create = async (req, res) => {
-    const {name, username, email, password, avatar, backgroud} = req.body;
+    const { name, username, email, password, avatar, backgroud } = req.body;
 
-    if (!name || !username || !email || !password || !avatar || !backgroud){
-        res.status(400).send({message: "submit all fields for registration"})
+    if (!name || !username || !email || !password || !avatar || !backgroud) {
+        res.status(400).send({ message: "submit all fields for registration" })
     }
 
     const user = await userService.create(req.body);
 
-    if (!user){
-        return res.status(400).send({message: "Error creating User "})
+    if (!user) {
+        return res.status(400).send({ message: "Error creating User " })
 
     }
 
@@ -26,8 +28,34 @@ const create = async (req, res) => {
             backgroud,
 
         },
-        
+
     })
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+    const users = await userService.findAllServices();
+
+    if (users.lengh === 0) {
+        return res.status(400).send({ message: "there are not registered users" });
+    }
+
+    res.send(users)
+};
+
+const findById = async (req, res) => {
+    const id = req.params.id
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).send({ messege: "invalid ID"})
+    }
+
+    const user = await userService.findByIdServices(id)
+
+    if (!user) {
+        return res.status(400).send({ messege: "User not found!" })
+    }
+
+    res.send(user)
+}
+
+module.exports = { create, findAll, findById};
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,6 +2,12 @@ const User = require('../models/User');
 
 const create = (body) => User.create(body);
 
+const findAllServices = () => User.find();
+
+const findByIdServices = (id) => User.findById(id);
+
 module.exports = {
     create,
+    findAllServices,
+    findByIdServices
 };
